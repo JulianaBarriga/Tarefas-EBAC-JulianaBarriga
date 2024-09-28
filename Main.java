349fadc5c40f47681da1a3aa2b15607d6e71d548
Juliana Barriga <165205681+JulianaBarriga@users.noreply.github.com>
2024-09-27 23:57:50 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double nota01 = 75.7, nota02 = 67.8, nota03 = 40.0, nota04 = 94.1;
+        double notasTotais = nota01 + nota02 + nota03 + nota04;
+
+        System.out.println("A média das notas é: " + notasTotais/4);
+    }
+}
